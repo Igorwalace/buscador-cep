2f692fb461d6d0fd8651f0eb71e7b244648a652e
igoor <igorwallacy3@gmail.com>
2024-04-17 11:12:37 -0300

    icon att

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
-import { Inter } from "next/font/google";
-import './styles/globals.css'
+import { Inter } from 'next/font/google';
+import './styles/globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Buscador de CEP",
-  description: "Projeto Buscador de CEP",
+    title: 'Buscador de CEP',
+    description: 'Projeto Buscador de CEP',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="pt-BR">
+            <head>
+                <link rel="icon" href="/logo.png" />
+            </head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
